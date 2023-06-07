3eace8e84dfe1db12d33fab3604f2e3c52a9bac8
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-06-07 15:05:58 -0500

    Add dialog box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import StarRating from './components/StarRating';
+import Dialog from './components/Dialog';
+import { useState } from 'react';
 
 function App() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleDialog = () => {
+    setDialogOpen(true);
+  }
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  }
+
   return (
     <div>
       <StarRating />
+        <button className='open-dialog-button' onClick={handleDialog}>Open Dialog</button>
+      <Dialog isOpen={dialogOpen} onClose={handleCloseDialog} />
     </div>
   );
 }
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Dialog = () => {
+const Dialog = ({ isOpen, onClose }) => {
   return (
-    <div>Dialog</div>
+    <>
+        {isOpen && (
+            <div className='dialog'>
+                <div className='dialog-content'>
+                    <h2>Hello!</h2>
+                    <p>I am a dialog box</p>
+                    <p>It's nice to meet you!</p>
+                    <button onClick={onClose}>Close</button>
+                </div>
+            </div>
+        )}
+    </>
   )
 }
 
